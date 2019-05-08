d1eff4d3986d765e7b1377b7ec956f595b442b67
Ben Gohlke <jcgohlke@me.com>
2019-05-08 13:20:48 -0400

    finished todocontroller

--- PutAndPost/Model Controllers/TodoController.swift
+++ PutAndPost/Model Controllers/TodoController.swift
@@ -15,14 +15,14 @@ enum PushMethod: String {
 
 class TodoController {
     
-    private let baseURL = URL(string: "")
+    private let baseURL = URL(string: "")!
     
     // MARK: Properties
     private(set) var todos: [Todo] = []
     
     func createTodo(with title: String) -> Todo {
         let todo = Todo(title: title)
-        
+        return todo
     }
     
     func push(todo: Todo, using method: PushMethod, completion: @escaping (Error?) -> Void) {
@@ -38,8 +38,25 @@ class TodoController {
         request.httpMethod = method.rawValue
         
         // encode data into JSON
+        do {
+            let encoder = JSONEncoder()
+            request.httpBody = try encoder.encode(todo)
+        } catch {
+            print(error)
+            completion(error)
+            return
+        }
         
         // send JSON to Firebase
+        URLSession.shared.dataTask(with: request) { (_, _, error) in
+            if let error = error {
+                print(error)
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+        }.resume()
     }
     
     func fetchTodos(completion: @escaping (Error?) -> Void) {
@@ -49,5 +66,28 @@ class TodoController {
         urlRequest.httpMethod = "GET"
         
         // fetch data from Firebase
+        URLSession.shared.dataTask(with: urlRequest) { (data, _, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            
+            do {
+                let decodedDictionary = try jsonDecoder.decode([String: Todo].self, from: data)
+                let todos = Array(decodedDictionary.values)
+                self.todos = todos
+            } catch {
+                print("Error decoding received data: \(error)")
+                completion(error)
+                return
+            }
+        }.resume()
     }
 }
